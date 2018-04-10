1ea75661cd3c50b83b4dc0ad611ac9deb8d710a3
boom <564296557@qq.com>
2018-04-10 19:00:33 +0800

    '添加自动化batch'

---
+++ plugin.bat
@@ -0,0 +1,14 @@
+@echo off
+set vimDir="%UserProfile%\test"
+if not exist %vimDir% mkdir %vimDir%
+if not exist "%vimDir%\autoload" mkdir "%vimDir%\autoload"
+if not exist "%vimDir%\autoload\pathogen.vim" (
+        bitsadmin.exe /transfer "download pathogen.vim" https://tpo.pe/pathogen.vim "%vimDir%\autoload\pathogen.vim"
+)
+if not exist "%vimDir%\bundle" mkdir "%vimDir%\bundle"
+
+for /f "delims=| tokens=1,2" %%G in (plugin-hub) do (
+        set %%G=%%H 
+        if not exist "%vimDir%\bundle\%%G" git clone %%H  "%vimDir%\bundle\%%G"
+)
+@pause
